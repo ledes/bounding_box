7a8f799b47a04b971319c45d9bd645c317252f34
Pablo Ledesma <pablo.ledesma.p@gmail.com>
2015-08-29 13:18:03 -0400

    BoundingArea class created

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -1,2 +1,19 @@
+require_relative '../lib/bounding_box'
+
 class BoundingArea
+
+  def initialize( boxes =[])
+    @boxes = boxes
+  end
+
+  def boxes_contain_point?(x, y)
+    flag = 0
+    @boxes.each do |box|
+      if  box.contains_point?(x, y)
+        flag += 1
+      end
+    end
+    flag > 0
+  end
+  
 end
