403dc692a921b0f56fdf9bb880e95f7a0ef29508
Pablo Ledesma <pablo.ledesma.p@gmail.com>
2015-08-31 10:08:40 -0400

    refractor using any method

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -1,19 +1,24 @@
 require_relative '../lib/bounding_box'
 
 class BoundingArea
+  attr_reader :boxes
 
   def initialize( boxes =[])
     @boxes = boxes
   end
+  #
+  # def boxes_contain_point?(x, y)
+  #   flag = false
+  #   @boxes.each do |box|
+  #     if  box.contains_point?(x, y)
+  #       flag = true
+  #     end
+  #   end
+  #   flag
+  # end
 
   def boxes_contain_point?(x, y)
-    flag = false
-    @boxes.each do |box|
-      if  box.contains_point?(x, y)
-        flag = true
-      end
-    end
-    flag
+    boxes.any?{|box| box.contains_point?(x,y)}
   end
 
 end
